07b8f9ecdbc15d5bcda37c746b7571c07d491194
HeeJu-K <kmju99@naver.com>
2023-02-20 17:36:02 -0800

    update local

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,2 +1,21 @@
-from pocketsphinx import LiveSpeech
-for phrase in LiveSpeech(): print(phrase)
+import speech_recognition as sr
+from gpiozero import LED
+from time import sleep
+
+red = LED(17)
+relay1 = LED(14)
+relay2 = LED(15)
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+print("hello")
+
+while True:
+    print("in loop")
+    with mic as source:
+        audio = r.record(source, duration=30)
+        print("audio: ", audio)
+    words = r.recognize_google(audio)
+    print("words", words)
+   
